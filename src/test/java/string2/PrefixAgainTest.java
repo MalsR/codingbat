176038f68a002f6b391562f1bcd9bef0660a0a62
MalsR <malinda_rajapakse@hotmail.com>
2016-01-17 13:08:25 +0000

    Added PrefixAgain example

---
+++ src/test/java/string2/PrefixAgainTest.java
@@ -0,0 +1,37 @@
+package string2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrefixAgainTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionWhenCharPrefixIsGreaterThanStringLength() {
+        PrefixAgain prefixAgain = new PrefixAgain();
+
+        prefixAgain.prefixAgain("Hello", 6);
+    }
+
+    @Test
+    public void returnsTrueWhenCalculatedStringPrefixAppearsAnywhereElseInString() {
+        PrefixAgain prefixAgain = new PrefixAgain();
+
+        assertTrue(prefixAgain.prefixAgain("Hellheo", 2));
+    }
+
+    @Test
+    public void returnsTrueWhenStringContainsSameCharsLengthOfTwoAndPrefixLengthIsOne() {
+        PrefixAgain prefixAgain = new PrefixAgain();
+
+        assertTrue(prefixAgain.prefixAgain("aa", 1));
+    }
+
+    @Test
+    public void returnsFalseWhenCalculatedStringPrefixDoesNotAppearAnywhereElseInString() {
+        PrefixAgain prefixAgain = new PrefixAgain();
+
+        assertFalse("Expected to return false since prefix does not contain anywhere else in String",
+                prefixAgain.prefixAgain("abXYabc", 3));
+    }
+}
