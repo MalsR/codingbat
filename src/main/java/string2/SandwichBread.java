7094da30cc58005a6d56dac0ac0877da26c79f98
MalsR <malinda_rajapakse@hotmail.com>
2016-01-27 21:05:07 +0000

    Added comment

--- src/main/java/string2/SandwichBread.java
+++ src/main/java/string2/SandwichBread.java
@@ -24,6 +24,8 @@ public class SandwichBread {
                 if (BREAD.equalsIgnoreCase(substring)) {
                     //Found instance of bread
                     breadIndexes.add(i);
+
+                    //Small change to skip unnecessary substring
                     //So if we found instance of bread and we don't need to do a substring of the chars in bread but
                     //jump to the end of brea'd' and start from the next char after. Note bread = 5 chars but
                     //i += 4 and back in the loop it increments by 1, hence 5
